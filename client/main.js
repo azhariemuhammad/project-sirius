d14bd345d3ee275ab8d200e3d1b53c44fbbc607b
azhariemuhammad <azharie_muhammad@live.com>
2017-11-17 01:22:53 +0700

    api google maps done

--- client/main.js
+++ client/main.js
@@ -1,6 +1,5 @@
-  //
-   $(document).ready(function () {
 
+   $(document).ready(function () {
        axios.get(`https://api.nasa.gov/planetary/apod?api_key=ULtXwM7iAjNU8wX4EeEyII970BuYKeJX166vSo0J`)
        .then(({data}) => {
          console.log(data);
@@ -13,20 +12,4 @@
        .catch(function (error) {
          console.log(error);
        })
-
-      //  axios.get(`http://api.open-notify.org/iss-now.json`)
-      //  .then(({data}) => {
-      //    console.log(data.iss_position);
-      //   //  $('#getPhoto').append(
-      //   //    `<p>${data.date}</p>
-      //   //    <img src=${data.url} style="">
-      //   //    <p>${data.explanation}</p>`
-      //   //  )
-      //  })
-      //  .catch(function (error) {
-      //    console.log(error);
-      //  })
-
-
-
     })
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-//
-// router.get('/apod', controller.getPhoto)
-//
-//
-// router.get('/currentlocation', controller.getPosition )
-//
-//
+
 
 
 module.exports = router
