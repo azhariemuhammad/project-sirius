de4b4402da98f03d34423152ed567c0c3fc33497
aditReza <lete.note3@gmail.com>
2017-11-16 17:53:46 +0700

    create template app

---
+++ server/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// mongoose connect
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/siriusProject')
+.then(() =>  console.log('db connection succesful'))
+.catch((err) => console.error(err));
+
+//routes
+const index = require('./routes/index')
+
+app.use('/', index)
+
+app.listen(3000, function(err){
+  if(!err) console.log('server listen on port | 3000')
+})
